6afa9a16e37a90e17a3dd48555867f7b21d9a03e
tongdung13 <tongvanthienvu@gmail.com>
2020-11-09 09:54:28 +0700

    ok

--- index.php
+++ index.php
@@ -1,15 +1,22 @@
-<?php
 
-$mang = array(
-    array(1,3,5,8,5,7,9),
-    array(3,5,7,98,34,5,1)
-);
-$max = $mang[0][0];
- for ($i = 0; $i < count($mang); $i++){
-     for ($j = 0; $j < count($mang[$i]); $j++) {
-         if ($max < $mang[$i][$j]) {
-             $max = $mang[$i][$j];
-         }
-     }
- }
- echo $max;
+<form method="post">
+    <input type="text" name="nhap" placeholder="mang1" >
+    <input type="text" name="nhap" placeholder="mang2"/>
+    <input type="submit" value="check">
+</form>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $mang = $_POST["nhap"];
+    $array = explode(",",$mang);
+    $max = $mang[0][0];
+    for ($i = 0; $i < count($array); $i++) {
+        for ($j = 0; $j < count($array[$i]); $j++) {
+            if ($max < $array[$i][$j]) {
+                $max = $array[$i][$j];
+            }
+        }
+    }
+    echo $max;
+
+}
